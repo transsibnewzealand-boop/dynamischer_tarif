4447b57fd6f48a51df16edd893480d6e9b11fccb
transsibnewzealand-boop <transsibnewzealand@gmail.com>
2026-05-21 15:45:47 +0200

    Update app.js

--- public/app.js
+++ public/app.js
@@ -227,7 +227,7 @@ function render(payload){
         }
       },
       scales: {
-        x: { ticks: { maxTicksLimit: 12, color: '#53657a' }, grid: { color: 'rgba(12,37,66,.06)' } },
+        x: { beginAtZero: true, ticks: { maxTicksLimit: 12, color: '#53657a' }, grid: { color: 'rgba(12,37,66,.06)' } },
         y: { ticks: { color: '#53657a' }, grid: { color: 'rgba(12,37,66,.06)' }, title: { display: true, text: 'Rp./kWh', color:'#53657a' } }
       }
     },
